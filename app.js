be489988843fc2bb721932d16c2581bf83b3b54c
Eyal Kobrigo on xbian <kobrigo@gmail.com>
2014-04-05 22:04:25 +0000

    Initial commit. Tests with leds making it turn on and off

---
+++ app.js
@@ -0,0 +1,40 @@
+//var gpio = require('rpi-gpio');
+//
+//gpio.on('export', function(channel) {
+//    console.log('Channel set: ' + channel);
+//});
+//
+////gpio.setup(7, gpio.DIR_OUT);
+////gpio.setup(15, gpio.DIR_OUT);
+//gpio.setup(11, gpio.DIR_OUT, pause);
+//
+//
+//function pause() {
+//    gpio.write(7, true);
+//    setTimeout(closePins, 3000);
+//}
+//
+//function closePins() {
+//    gpio.destroy(function() {
+//        console.log('All pins unexported');
+//        return process.exit(0);
+//    });
+//}
+
+var gpio = require("pi-gpio");
+var pinToWorkOn = 7;
+gpio.open(pinToWorkOn, "output", function(err) { 
+
+    console.log("error: " + err);
+
+    gpio.write(pinToWorkOn, 1, function() {
+        setTimeout(function() { 
+            console.log("closing pin: " + pinToWorkOn);
+            gpio.write(pinToWorkOn, 0, function() {
+            	gpio.close(pinToWorkOn);	
+            });
+//            gpio.close(pinToWorkOn);
+            
+        }, 3000);
+    });
+});
